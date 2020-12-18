3bcd21ed5f0e1be9ae743b9fb59a4889bb415dec
cooper2187 <69003500+cooper2187@users.noreply.github.com>
2020-12-18 20:19:27 +0200

    Update aid.py

--- cogs/aid.py
+++ cogs/aid.py
@@ -20,7 +20,7 @@ class Aid(commands.Cog):
             return
         if message.content.startswith('$help$'):
             await message.delete()
-            emb = discord.Embed(description = '**Список команд:** ' + '`{}command`\n'.format(prefix) + '**Help: [Support Server](https://discord.gg/sWHrXQT)\nAdd Opium: [Invite Opium to your Server](https://discord.com/api/oauth2/authorize?client_id=722921602026700861&permissions=8&scope=bot)**')
+            emb = discord.Embed(description = '**Список команд:** ' + '`{}commands`\n'.format(prefix) + '**Help: [Support Server](https://discord.gg/sWHrXQT)\nAdd Opium: [Invite Opium to your Server](https://discord.com/api/oauth2/authorize?client_id=722921602026700861&permissions=8&scope=bot)**')
             emb.set_author(name = f"Префикс на {message.guild.name} =  ' {prefix} '", icon_url = message.guild.icon_url)
             await message.author.send(embed = emb)
         if message.content.startswith('prefix'):
@@ -36,8 +36,8 @@ class Aid(commands.Cog):
         await ctx.author.send(embed = emb)
 
     #COMMAND
-    @commands.command()
-    async def command(self, ctx):
+    @commands.command(aliases = ["commands"])
+    async def __commands(self, ctx):
         prefix = self.prx.find_one({"_id": ctx.guild.id})["prefix"]
         emb = discord.Embed ( title = '🌴 Команды Opium 🌴', color = 0x17a891, description = '🔰 **Пользовательские команды:**\n\n🔹 **{}info - **'.format(prefix) + 'Информация  о пользователе\n🔹 **{}serverinfo - **'.format(prefix) + 'Информация о сервере\n🔹 **{}changenick - **'.format(prefix) + 'Смена ника на сервере\n🔹 **{}time - **'.format(prefix) + 'Дата и время (MSK)\n🔹 **{}privat - **'.format(prefix) + 'Создание приватной голосовой комнаты на две персоны\n\n🔰 **Для модераторов:**\n\n🔹 **{}clear - **'.format(prefix) + 'Очистка чата\n🔹 **{}setnick - **'.format(prefix) + 'Сменить ник пользователю\n🔹 **{}mute - **'.format(prefix) + 'Ограничить доступ к текстовым и голосовым чатам\n🔹 **{}unmute - **'.format(prefix) + 'Снять ограничение к текстовым и голосовым чатам\n🔹 **{}kick - **'.format(prefix) + 'Выгнать участника с сервера\n🔹 **{}ban - **'.format(prefix) + 'Забанить участника\n🔹 **{}setmoderator - **'.format(prefix) + 'Назначить пользователя модератором')
         await ctx.message.delete()
