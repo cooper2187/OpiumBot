020fc4881d86a6fcc9d3b669d8ceef59ff9d2b9e
cooper2187 <69003500+cooper2187@users.noreply.github.com>
2020-12-28 13:02:26 +0200

    Update bigdick.py

--- cogs/bigdick.py
+++ cogs/bigdick.py
@@ -16,7 +16,6 @@ class Dick(commands.Cog):
         self.game = self.cluster.opiumdb.dsdickcoll
         self.prx = self.cluster.opiumdb.prefixcoll
 
-    #ON MEMBER JOIN
     @commands.Cog.listener()
     async def on_member_join(self, member):
         if not self.game.count_documents({"guild_id": member.guild.id, "user_id": member.id}) == 0:
@@ -24,7 +23,6 @@ class Dick(commands.Cog):
         else:
             pass
 
-    #ON MEMBER REMOVE
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if not self.game.count_documents({"guild_id": member.guild.id, "user_id": member.id}) == 0:
@@ -32,25 +30,38 @@ class Dick(commands.Cog):
         else:
             pass
 
-    #ON GUILD JOIN
+
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         ch = {
             "guild_id": guild.id,
             "guild_name": guild.name,
-            "channel_id": 0
+            "channel_id": 0,
+            "lang": "Rus"
         }
         if self.game.count_documents({"guild_id": guild.id}) == 0:
             self.game.insert_one(ch)
 
-    #SET CHANNEL
     @commands.command()
     async def set_channel(self, ctx, channel: discord.TextChannel):
         self.game.update_one({"guild_id": ctx.guild.id}, {"$set": {"channel_id": channel.id}})
 
-    #DICK
+
+    @commands.command()
+    async def set_lang(self, ctx, lang = None):
+        prefix = self.prx.find_one({"_id": ctx.guild.id})["prefix"]
+        if lang is None:
+            await ctx.send(embed = discord.Embed(description = f'**{prefix}set_lang [rus/ukr/eng]** - Установить язык(Русский/Украинский/Английский)\n**{prefix}set_lang rus**', color = 0x667676))
+        elif lang == "rus":
+            self.game.update_one({"guild_id": ctx.guild.id}, {"$set": {"lang": "Rus"}})
+        elif lang == "urk":
+            self.game.update_one({"guild_id": ctx.guild.id}, {"$set": {"lang": "Urk"}})
+        else:
+            await ctx.send(embed = discord.Embed(description = f'**{prefix}set_lang [rus/ukr]** - Установить язык(Русский/Украинский)\n**{prefix}set_lang rus**', color = 0x667676))    
+
     @commands.command()
     async def dick(self, ctx):
+        lang = self.game.find_one({"guild_id": ctx.guild.id})["lang"]
         if not ctx.channel.id == self.game.find_one({"guild_id": ctx.guild.id})["channel_id"]:
             return
         new = {
@@ -63,7 +74,10 @@ class Dick(commands.Cog):
         }
         if self.game.count_documents({"guild_id": ctx.guild.id, "user_id": ctx.author.id}) == 0:
             self.game.insert_one(new)
-            await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ти зареєструвався у грі "Найдовший песюн!"**', color = 0x0073fe))
+            if lang == "Rus":
+                await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ты зарегистрировался в игре "Самый длинный песюн!"**', color = 0x0073fe))
+            else:
+                await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ти зареєструвався у грі "Найдовший песюн!"**', color = 0x0073fe))
         data = self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})
         delta = datetime.timedelta(hours=2, minutes=0)
         now = datetime.datetime.now() + delta
@@ -71,7 +85,10 @@ class Dick(commands.Cog):
         d = date - now
         hr = time.strftime("%H", time.gmtime(d.seconds))
         mins = time.strftime("%M", time.gmtime(d.seconds))
-        tt = '{} год. {} хв.'.format(int(hr), int(mins) + 1)
+        if lang == "Rus":
+            tt = '{} ч. {} мин.'.format(int(hr), int(mins) + 1)
+        else:
+            tt = '{} год. {} хв.'.format(int(hr), int(mins) + 1)
         n = randint(1, 20)
         lplus = randint(1, 10)
         lminus = randint(-5, -1)
@@ -80,49 +97,71 @@ class Dick(commands.Cog):
         if data["in_game"] == 0:
             pass
         else:
-            await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ти повернувся до гри!**", color = 0x0073fe))
+            if lang == "Ukr":
+                await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ти повернувся до гри!**", color = 0x0073fe))
+            else:
+                await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ты вернулся к игре!**", color = 0x0073fe))
             self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"in_game": 0}})
         if data["status"] == 0:
             if n in a:
                 self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$inc": {"len": lplus}})
-                e = discord.Embed(description = f'**Твій песюн виріс на {lplus} см. 😎\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
+                if lang == "Ukr":
+                    e = discord.Embed(description = f'**Твій песюн виріс на {lplus} см. 😎\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
+                else:
+                    e = discord.Embed(description = f'**Твой песюн вырос на {lplus} см. 😎\nТеперь его длина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродолжай играть через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00) 
                 e.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
                 await ctx.send(embed = e)
                 self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"status": 1}})
             elif n in b:
                 if data['len'] == 0:
-                    e = discord.Embed(description = f'**В тебе немає песюна. 😧\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0xffa000)
+                    if lang == "Ukr":
+                        e = discord.Embed(description = f'**В тебе немає песюна. 😧\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0xffa000)
+                    else:
+                        e = discord.Embed(description = f'**У тебя нету песюна. 😧\nПродолжай играть через {tt}**', timestamp = ctx.message.created_at, color = 0xffa000)
                     e.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
                     await ctx.send(embed = e)
                     self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"status": 1}})
                 elif -lminus >= data["len"]:
                     self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$inc": {"len": 1}})
-                    e = discord.Embed(description = f'**Твій песюн виріс на 1 см. 😎\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
+                    if lang == "Ukr":
+                        e = discord.Embed(description = f'**Твій песюн виріс на 1 см. 😎\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
+                    else:
+                        e = discord.Embed(description = f'**Твой песюн вырос на 1 см. 😎\nТеперь его длина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродолжай играть через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
                     e.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
                     await ctx.send(embed = e)
                     self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"status": 1}})
                 else:
                     self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$inc": {"len": lminus}})
-                    e = discord.Embed(description = f'**Твій песюн скоротився на {-lminus} см. 🤣\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0xff1c00)
+                    if lang == "Ukr":
+                        e = discord.Embed(description = f'**Твій песюн скоротився на {-lminus} см. 🤣\nТепер його довжина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродовжуй грати через {tt}**', timestamp = ctx.message.created_at, color = 0xff1c00)
+                    else:
+                        e = discord.Embed(description = f'**Твой песюн уменьшился на {-lminus} см. 🤣\nТеперь его длина: {self.game.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})["len"]} см.\nПродолжай играть через {tt}**', timestamp = ctx.message.created_at, color = 0x26cb00)
                     e.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
                     await ctx.send(embed = e)
                     self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"status": 1}})
             else:
                 return
         else:
-            await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ти сьогодні вже грав!**', color = 0xff0000))
+            if lang == "Ukr":
+                await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ти сьогодні вже грав!**', color = 0xff0000))
+            else:
+                await ctx.send(embed = discord.Embed(description = f'**{ctx.author.mention}, ты сегодня уже играл!**', color = 0xff0000))
 
-    #UNDICK
     @commands.command()
     async def undick(self, ctx):
+        lang = self.game.find_one({"guild_id": ctx.guild.id})["lang"]
         if not ctx.channel.id == self.game.find_one({"guild_id": ctx.guild.id})["channel_id"]:
             return
         self.game.update_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id}, {"$set": {"in_game": 1}})
-        await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ти вийшов з гри!**", color = 0x0073fe))
+        if lang == "Ukr":
+            await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ти вийшов з гри!**", color = 0x0073fe))
+        else:
+            await ctx.send(embed = discord.Embed(description = f"**{ctx.author.mention}, ты вышел из игры!**", color = 0x0073fe))
 
-    #TOP PLAYERS
+
     @commands.command()
     async def top(self, ctx, count: int = None):
+        lang = self.game.find_one({"guild_id": ctx.guild.id})["lang"]
         if not ctx.channel.id == self.game.find_one({"guild_id": ctx.guild.id})["channel_id"]:
             return
         if count is None:
@@ -135,12 +174,18 @@ class Dick(commands.Cog):
         for t in top:
             member = self.client.get_guild(ctx.guild.id).get_member(t['user_id'])
             if t['len'] == 0:
-                leaders.append(f'**🔹 {i}. {member.display_name} — немає песюна**')
+                if lang == "Ukr":    
+                    leaders.append(f'**🔹 {i}. {member.display_name} — немає песюна**')
+                else:
+                    leaders.append(f'**🔹 {i}. {member.display_name} — нету песюна**')
             else:
                 leaders.append(f'**🔹 {i}. {member.display_name} — {t["len"]} см**')
             i = i + 1
         if len(leaders) <= 0:
-            await ctx.send(embed = discord.Embed(description = f'**Ніхто не грає...**', color = 0x667676))
+            if lang == "Ukr":
+                await ctx.send(embed = discord.Embed(description = f'**Ніхто не грає...**', color = 0x667676))
+            else:
+                await ctx.send(embed = discord.Embed(description = f'**Никто не играет...**', color = 0x667676))
         else:
             e = discord.Embed(description = "\n".join(leaders), color = 0x32aafd, timestamp = ctx.message.created_at)
             e.set_author(name = f'{ctx.guild.name}| Leaderboard', icon_url = ctx.guild.icon_url)
