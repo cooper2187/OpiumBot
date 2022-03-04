9d7b1ca754cc0173e1b53301d1e460184d313b62
cooper2187 <69003500+cooper2187@users.noreply.github.com>
2022-03-04 14:30:49 +0200

    Update worktime.py

--- cogs/worktime.py
+++ cogs/worktime.py
@@ -32,20 +32,17 @@ class Worktime(commands.Cog):
                         "name": f"{payload.member.name}#{payload.member.discriminator}",
                         "come": 0,
                         "leave": 0,
-                        f"worktime{nw.month}": [0], 
+                        f"worktime{nw.month}": [None], 
                         f"total{nw.month}": 0
                     }
                     if self.wt.count_documents({"id": payload.member.id}) == 0:
                         self.wt.insert_one(info)
-                    if nw.day == 1:
-                        self.wt.update_one({"id": payload.member.id}, {"$set": {f"worktime{nw.month}": [None]}})
-                        self.wt.update_one({"id": payload.member.id}, {"$set": {f"total{nw.month}": 0}})
                     if self.wt.find_one({"id": payload.member.id})["come"] != "0":
                         await payload.member.send("Error!")
                     else:
                         self.wt.update_one({"id": payload.member.id}, {"$set": {"come": f"{datetime.datetime.now()}"}})
                         e = discord.Embed(description = f'Дата: **{datetime.datetime.now().strftime("%d.%m.%Y")}**\nВремя: **{datetime.datetime.now().strftime("%H:%M")}**', color = 0x02ff00)
-                        e.set_author(name = "VARUS | Приход", icon_url = "https://cdn.discordapp.com/attachments/735452352336756808/928601669686685716/213a003b270cf11f.jpg")
+                        e.set_author(name = "VARUS | Вход", icon_url = "https://cdn.discordapp.com/attachments/735452352336756808/928601669686685716/213a003b270cf11f.jpg")
                         await payload.member.send(embed = e)
                 except discord.ext.commands.errors.CommandInvokeError:
                     print("Error")
@@ -67,13 +64,13 @@ class Worktime(commands.Cog):
                         a1 = aa.strftime("%H:%M")
                         a2 = aa.strftime("%d.%m.%Y")
                         bb = datetime.datetime.strptime(b, "%Y-%m-%d %H:%M:%S.%f")
-                        delta = datetime.timedelta(minutes = 90)
+                        delta = datetime.timedelta(minutes = 60)
                         c = bb - aa - delta
                         cc = datetime.datetime.strptime(str(c), "%H:%M:%S.%f")
                         ttl = self.rounding(cc.hour + round(cc.minute/60, 1))
-                        e = discord.Embed(description = f'Дата: **{a2}**\nВермя прихода: **{a1}**\nВремя ухода: **{datetime.datetime.now().strftime("%H:%M")}**\nОтработано: **{cc.strftime("%H:%M")}**', color = 0xff0000)
+                        e = discord.Embed(description = f'Дата: **{a2}**\nВход: **{a1}**\nВыход: **{datetime.datetime.now().strftime("%H:%M")}**\nОтработано: **{cc.strftime("%H:%M")}**', color = 0xff0000)
                         e.set_author(name = "VARUS | Уход", icon_url = "https://cdn.discordapp.com/attachments/735452352336756808/928601669686685716/213a003b270cf11f.jpg")
-                        self.wt.update_one({"id": payload.member.id}, {"$push": {f"worktime{nw.month}": f"{a2}. Приход: {a1} | Уход: {datetime.datetime.now().strftime('%H:%M')} | Отработано: {cc.strftime('%H:%M')}"}})
+                        self.wt.update_one({"id": payload.member.id}, {"$push": {f"worktime{nw.month}": f"{a2}. Вход: {a1} | Выход: {datetime.datetime.now().strftime('%H:%M')} | Отработано: {cc.strftime('%H:%M')}"}})
                         self.wt.update_one({"id": payload.member.id}, {"$inc": {f"total{nw.month}": ttl}})
                         self.wt.update_one({"id": payload.member.id}, {"$set": {"come": "0"}})
                         self.wt.update_one({"id": payload.member.id}, {"$set": {"leave": "0"}})
